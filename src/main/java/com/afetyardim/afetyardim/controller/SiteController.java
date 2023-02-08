9bc700c7ce33c286286be1d1a94830404697e34d
Nurettin Akyüz <nakyuz@atlassian.com>
2023-02-08 23:49:07 +0300

    Get all sites with updates

--- src/main/java/com/afetyardim/afetyardim/controller/SiteController.java
+++ src/main/java/com/afetyardim/afetyardim/controller/SiteController.java
@@ -1,20 +1,13 @@
 package com.afetyardim.afetyardim.controller;
 
-import com.afetyardim.afetyardim.dto.SiteListDTO;
 import com.afetyardim.afetyardim.model.Site;
 import com.afetyardim.afetyardim.model.SiteUpdate;
 import com.afetyardim.afetyardim.service.SiteService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Collection;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sites")
@@ -31,9 +24,9 @@ public class SiteController {
   }
 
   @GetMapping
-  public Collection<SiteListDTO> getSites(@RequestParam Optional<String> cityFilter) {
+  public Collection<Site> getSites(@RequestParam Optional<String> cityFilter) {
     Collection<Site> sites = siteService.getSites(cityFilter);
-    return convertModelsToDTO(sites);
+    return sites;
   }
 
   @GetMapping("/{siteId}")
@@ -47,23 +40,5 @@ public class SiteController {
     newSiteUpdate.setCreateDateTime(null);
     return siteService.addSiteUpdate(siteId, newSiteUpdate);
   }
-
-  private Collection<SiteListDTO> convertModelsToDTO(Collection<Site> sites) {
-    return sites.stream().map(this::convertModelToDTO).collect(Collectors.toSet());
-  }
-
-  private SiteListDTO convertModelToDTO(Site site) {
-    SiteListDTO siteListDTO = new SiteListDTO();
-    siteListDTO.setId(site.getId());
-    siteListDTO.setCreateDateTime(site.getCreateDateTime());
-    siteListDTO.setName(site.getName());
-    siteListDTO.setLocation(site.getLocation());
-    siteListDTO.setOrganizer(site.getOrganizer());
-    siteListDTO.setDescription(site.getOrganizer());
-    siteListDTO.setContactInformation(site.getContactInformation());
-    siteListDTO.setLastSiteStatus(site.getLastSiteStatus());
-    return siteListDTO;
-  }
-
 
 }
