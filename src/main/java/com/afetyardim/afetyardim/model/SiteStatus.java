96ba0990ce894c345b2a43c33c37843f3670990e
cagataysel <cagatays@shipmercury.com>
2023-02-10 10:45:04 +0300

    Add jackson ignores

--- src/main/java/com/afetyardim/afetyardim/model/SiteStatus.java
+++ src/main/java/com/afetyardim/afetyardim/model/SiteStatus.java
@@ -1,5 +1,6 @@
 package com.afetyardim.afetyardim.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Embeddable
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SiteStatus {
 
   private SiteStatusType siteStatusType;
