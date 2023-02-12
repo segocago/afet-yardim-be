f5a5ebc294d425ec78e671dcc5c65e1aa751ac28
cagataysel <cagatays@shipmercury.com>
2023-02-12 14:48:47 +0300

    Add activeStatus enum, update ankara parser

--- src/main/java/com/afetyardim/afetyardim/model/Site.java
+++ src/main/java/com/afetyardim/afetyardim/model/Site.java
@@ -3,16 +3,24 @@ package com.afetyardim.afetyardim.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.vladmihalcea.hibernate.type.json.JsonType;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Entity
@@ -67,7 +75,12 @@ public class Site {
   private boolean verified = false;
 
   @Column(columnDefinition = "boolean default false")
+  @Deprecated
   private Boolean active = false;
+
+  @Column(length = 32, columnDefinition = "varchar(255) default 'UNKNOWN'")
+  @Enumerated(EnumType.STRING)
+  private ActiveStatus activeStatus = ActiveStatus.UNKNOWN;
 
 
   public void addSiteUpdate(SiteUpdate siteUpdate) {
@@ -75,20 +88,6 @@ public class Site {
       updates = new ArrayList<>();
     }
     updates.add(siteUpdate);
-
   }
-
-  public List<SiteStatus> getLastSiteStatuses() {
-    if (lastSiteStatuses == null || lastSiteStatuses.isEmpty()) {
-      lastSiteStatuses = List.of(
-              new SiteStatus(SiteStatusType.HUMAN_HELP, SiteStatus.SiteStatusLevel.NO_NEED_REQUIRED),
-              new SiteStatus(SiteStatusType.FOOD, SiteStatus.SiteStatusLevel.NO_NEED_REQUIRED),
-              new SiteStatus(SiteStatusType.MATERIAL, SiteStatus.SiteStatusLevel.NO_NEED_REQUIRED),
-              new SiteStatus(SiteStatusType.PACKAGE, SiteStatus.SiteStatusLevel.NO_NEED_REQUIRED)
-      );
-    }
-    return lastSiteStatuses;
-  }
-
 
 }
