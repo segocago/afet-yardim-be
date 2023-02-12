3c520eb58ac68ba708e3fe4a2c6f54c115467095
segocago <selcagatay1@hotmail.com>
2023-02-12 21:38:37 +0300

    Add istanbul parser

--- src/main/java/com/afetyardim/afetyardim/service/PeriodicalSpreadSheetUpdater.java
+++ src/main/java/com/afetyardim/afetyardim/service/PeriodicalSpreadSheetUpdater.java
@@ -1,5 +1,6 @@
 package com.afetyardim.afetyardim.service;
 
+import com.afetyardim.afetyardim.service.istanbul.IstanbulSitesParser;
 import com.afetyardim.afetyardim.service.izmir.IzmirSitesParser;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
@@ -12,16 +13,17 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class PeriodicalSpreadSheetUpdater {
 
-  private final long ANKARA_UPDATE_PERIOD_IN_MILLIS = 5 * 60 * 1000;
+  private final long PARSER_PERIOD_IN_MILLIS = 10 * 60 * 1000;
   private final long INITIAL_SCHEDULED_JOB_DELAY_IN_MILLIS = 30 * 1000;
 
   private final AnkaraGoogleSheetsService ankaraGoogleSheetsService;
-
   private final IzmirSitesParser izmirSitesParser;
+  private final IstanbulSitesParser istanbulSitesParser;
 
-  @Scheduled(initialDelay = INITIAL_SCHEDULED_JOB_DELAY_IN_MILLIS, fixedRate = ANKARA_UPDATE_PERIOD_IN_MILLIS)
+  @Scheduled(initialDelay = INITIAL_SCHEDULED_JOB_DELAY_IN_MILLIS, fixedRate = PARSER_PERIOD_IN_MILLIS)
   public void scheduleFixedRateTask() throws IOException {
 
+    parseIstanbulSpreadsheet();
     parseAnkaraSpreadsheet();
     parseIzmirSpreadsheet();
   }
@@ -46,14 +48,13 @@ public class PeriodicalSpreadSheetUpdater {
     log.info("Finish izmir spreadsheet parsing");
   }
 
-
-//  private void parseIstanbulSpreadsheet() {
-//    log.info("Start istanbul spreadsheet parsing");
-//    try {
-//      istanbulSpreadSheetParser.parseSpreadsheet();
-//    } catch (IOException e) {
-//      throw new RuntimeException("Exception while parsing izmir spreadsheet", e);
-//    }
-//    log.info("Finish istanbul spreadsheet parsing");
-//  }
+  private void parseIstanbulSpreadsheet() {
+    log.info("Start istanbul spreadsheet parsing");
+    try {
+      istanbulSitesParser.parseSpreadsheet();
+    } catch (IOException e) {
+      throw new RuntimeException("Exception while parsing izmir spreadsheet", e);
+    }
+    log.info("Finish istanbul spreadsheet parsing");
+  }
 }
