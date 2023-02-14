88a03de36f04f9c163074b91ae96b305f021838c
Cagatay Sel <cagatays@shipmercury.com>
2023-02-14 23:21:16 +0300

    Comment out site update create get apis, leave only list apis

--- src/main/java/com/afetyardim/afetyardim/controller/SiteController.java
+++ src/main/java/com/afetyardim/afetyardim/controller/SiteController.java
@@ -3,17 +3,12 @@ package com.afetyardim.afetyardim.controller;
 import com.afetyardim.afetyardim.dto.SiteDTO;
 import com.afetyardim.afetyardim.mapper.SiteMapper;
 import com.afetyardim.afetyardim.model.Site;
-import com.afetyardim.afetyardim.model.SiteUpdate;
 import com.afetyardim.afetyardim.service.SiteService;
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,12 +20,12 @@ public class SiteController {
 
   private final SiteService siteService;
 
-  @PostMapping
-  public Site createSite(@RequestBody Site newSite) {
-    newSite.setId(0);
-    newSite.setCreateDateTime(null);
-    return siteService.createSite(newSite);
-  }
+//  @PostMapping
+//  public Site createSite(@RequestBody Site newSite) {
+//    newSite.setId(0);
+//    newSite.setCreateDateTime(null);
+//    return siteService.createSite(newSite);
+//  }
 
   @GetMapping
   public Collection<Site> getSites(@RequestParam Optional<String> cityFilter, Optional<Boolean> verifiedFilter) {
@@ -38,16 +33,16 @@ public class SiteController {
     return sites;
   }
 
-  @GetMapping("/{siteId}")
-  public Site getSite(@PathVariable long siteId) {
-    return siteService.getSite(siteId);
-  }
+//  @GetMapping("/{siteId}")
+//  public Site getSite(@PathVariable long siteId) {
+//    return siteService.getSite(siteId);
+//  }
 
-  @PostMapping("/{siteId}/updates")
-  public Site addSiteUpdate(@PathVariable long siteId, @RequestBody SiteUpdate newSiteUpdate) {
-    newSiteUpdate.setCreateDateTime(LocalDateTime.now());
-    return siteService.addSiteUpdate(siteId, newSiteUpdate);
-  }
+//  @PostMapping("/{siteId}/updates")
+//  public Site addSiteUpdate(@PathVariable long siteId, @RequestBody SiteUpdate newSiteUpdate) {
+//    newSiteUpdate.setCreateDateTime(LocalDateTime.now());
+//    return siteService.addSiteUpdate(siteId, newSiteUpdate);
+//  }
 
   @GetMapping("/v2")
   public List<SiteDTO> getSites(@RequestParam Optional<String> cityFilter) {
